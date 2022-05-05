38b2662cc07dc966e3da4beb03fa5fd80f2bdc3a
A. Koepcke <akoepcke@users.noreply.github.com>
2022-05-05 09:37:59 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
 function App() {
-  return <div className="bg-blue-600">Test</div>;
+  const constraintsRef = useRef(null);
+
+  return (
+    <div className="bg-gradient-to-br from-cyan-300 to-blue-500 w-screen h-[100svh] fixed overflow-hidden overscroll-none p-10">
+      <div className="w-full h-full border-4" ref={constraintsRef}>
+        <motion.div
+          className="w-16 h-16 bg-pink-800"
+          drag
+          whileTap={{ scale: 1.3 }}
+          whileDrag={{ opacity: 0.5 }}
+          dragConstraints={constraintsRef}
+        ></motion.div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
